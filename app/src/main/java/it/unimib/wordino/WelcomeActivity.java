c8973b2231a9c24dfc5775e927340cb47454cb6e
richbira <rabijrichard@gmail.com>
2023-11-15 22:19:20 +0100

    added welcomeActivity

--- app/src/main/java/it/unimib/wordino/WelcomeActivity.java
+++ app/src/main/java/it/unimib/wordino/WelcomeActivity.java
@@ -3,17 +3,12 @@ package it.unimib.wordino;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
-        Log.d(TAG, "onCreate():");
     }
-    //TODO tutto il progetto
 }
