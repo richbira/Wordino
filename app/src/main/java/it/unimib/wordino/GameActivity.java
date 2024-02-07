9693971bc67c9a98831a726e98852b0a682a10d5
Penguolon <yoshiki.breznay@gmail.com>
2024-02-07 16:47:27 +0100

    bottone back su gameactivity

--- app/src/main/java/it/unimib/wordino/GameActivity.java
+++ app/src/main/java/it/unimib/wordino/GameActivity.java
@@ -2,10 +2,14 @@ package it.unimib.wordino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
+
     private static final String TAG = GameActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,14 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        ;
+        Button BackButton = findViewById(R.id.backButton);
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Play cliccato");
+                Intent intent = new Intent(view.getContext(), WelcomeActivity.class);
+                view.getContext().startActivity(intent);}
+        });
+
     }
 }
